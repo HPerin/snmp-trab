f7a56b443389b2f2b9178fb2d8e3b8062080bc38
Lucas Perin <lhperin@gmail.com>
2016-12-02 15:47:20 -0800

    working agent

---
+++ check_service.sh
@@ -0,0 +1,14 @@
+OUTPUT=`ps -eo pid,comm,lstart,etime,time,args | grep $1`
+SERVICE_NAME=`echo $OUTPUT | cut -d ' ' -f 2`
+
+if [ "$SERVICE_NAME" == "$1" ]; then
+    SERVICE_UPTIME=`echo $OUTPUT | cut -d ' ' -f 8`
+    SERVICE_PATH=`echo $OUTPUT | cut -d ' ' -f 10`
+
+    echo "1"
+    echo $SERVICE_NAME
+    echo $SERVICE_UPTIME
+    echo $SERVICE_PATH
+else
+    echo "0"
+fi
---
+++ simple_agent.py
@@ -0,0 +1,125 @@
+import sys, os, signal
+import optparse
+import pprint
+
+sys.path.insert(0, os.path.dirname(os.getcwd()))
+import netsnmpagent
+
+prgname = sys.argv[0]
+
+parser = optparse.OptionParser()
+parser.add_option(
+    "-m",
+    "--mastersocket",
+    dest="mastersocket",
+    help="Sets the transport specification for the master agent's AgentX socket",
+    default="/var/agentx/master"
+)
+parser.add_option(
+    "-p",
+    "--persistencedir",
+    dest="persistencedir",
+    help="Sets the path to the persistence directory",
+    default="/var/lib/net-snmp"
+)
+
+(options, args) = parser.parse_args()
+
+# rows, columns = os.popen("stty size", "r").read().split()
+rows = 10
+columns = 80
+
+try:
+    agent = netsnmpagent.netsnmpAgent(
+        AgentName="SERVICE-HEALTH",
+        MasterSocket=options.mastersocket,
+        PersistenceDir=options.persistencedir,
+        MIBFiles=[os.path.abspath(os.path.dirname(sys.argv[0])) +
+                  "/service.mib"]
+    )
+except netsnmpagent.netsnmpAgentException as e:
+    print("{0}: {1}".format(prgname, e))
+    sys.exit(1)
+
+serviceTable = agent.Table(
+    oidstr="SERVICE-HEALTH::serviceTable",
+    indexes=[
+        agent.Integer32()
+    ],
+    columns=[
+        (2, agent.OctetString("UnknownName")),
+        (3, agent.OctetString("UnknownState")),
+        (4, agent.OctetString("")),
+        (5, agent.OctetString(""))
+    ],
+    counterobj=agent.Integer32(
+        oidstr="SERVICE-HEALTH::serviceTableCounter"
+    ),
+    extendable=True
+)
+
+serviceTableSnmpd = serviceTable.addRow([agent.Integer32(0)])
+serviceTableSnmpd.setRowCell(2, agent.OctetString("snmpd"))
+serviceTableSnmpd.setRowCell(3, agent.OctetString(""))
+serviceTableSnmpd.setRowCell(4, agent.OctetString(""))
+serviceTableSnmpd.setRowCell(5, agent.OctetString(""))
+
+simpleCounter = agent.Integer32(
+    oidstr="SERVICE-HEALTH::simpleCounter",
+    writable=True
+)
+
+try:
+    agent.start()
+except netsnmpagent.netsnmpAgentException as e:
+    print("{0}: {1}".format(prgname, e))
+    sys.exit(1)
+
+print("{0}: AgentX connection to snmpd established.".format(prgname))
+
+
+def DumpRegistered():
+    for context in agent.getContexts():
+        print("{0}: Registered SNMP objects in Context \"{1}\": ".format(prgname, context))
+        vars = agent.getRegistered(context)
+        pprint.pprint(vars, width=columns)
+        print
+
+
+DumpRegistered()
+
+
+def TermHandler(signum, frame):
+    global loop
+    loop = False
+
+
+signal.signal(signal.SIGINT, TermHandler)
+signal.signal(signal.SIGTERM, TermHandler)
+
+
+def HupHandler(signum, frame):
+    DumpRegistered()
+
+
+signal.signal(signal.SIGHUP, HupHandler)
+
+print("{0}: Serving SNMP requests, send SIGHUP to dump SNMP object state, press ^C to terminate...".format(prgname))
+
+loop = True
+import subprocess
+while (loop):
+    # Block and process SNMP requests, if available
+    agent.check_and_process()
+
+    output = subprocess.Popen(["bash", "check_service.sh", "snmpd"], stdout=subprocess.PIPE).communicate()[0]
+    output = output.split('\n', -1)
+    if output[0] == '1':
+        serviceTableSnmpd.setRowCell(3, agent.OctetString("running"))
+        serviceTableSnmpd.setRowCell(4, agent.OctetString(output[2]))
+        serviceTableSnmpd.setRowCell(5, agent.OctetString(output[3]))
+    else:
+        serviceTableSnmpd.setRowCell(3, agent.OctetString("stopped"))
+
+print("{0}: Terminating.".format(prgname))
+agent.shutdown()
